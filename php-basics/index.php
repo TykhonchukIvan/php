9c1782583c818faa78089c36bde0bebfdcab4e07
TykhonchukIvan <tykhonchukivan@gmail.com>
2020-05-05 23:54:47 +0300

    'php-basics-path2'

--- php-basics/index.php
+++ php-basics/index.php
@@ -1,5 +1,8 @@
-<pre>
+<div style="margin: 20px auto; max-width: 600px; padding: 10px; background: bisque; ">
 <?php
+
+include_once "php-function.php";
+
     echo "Hello word<br>";
     $a = 10;
     echo "$a<br>";
@@ -39,9 +42,9 @@
     $null_1 = NULL;
 
     if (isset($null_1))
-        echo " Переменная a определена";
+        echo " Переменная  определена";
     else
-        echo " Переменная a не определена";
+        echo " Переменная  не определена";
 
     // string
 
@@ -65,6 +68,43 @@
 
     echo "Cтрока " . __LINE__ . " в файле " . __FILE__;
 
+    $int_1 = 1000;
+    $double_2 = 15.15151;
+    $string_3 = "abcd";
+
+    echo "<br>";
+    echo gettype($int_1);
+    echo "<br>";
+    echo gettype($double_2);
+    echo "<br>";
+    echo gettype($string_3);
+
+    settype($double_2, "integer");
+    echo "<br>";
+    echo $double_2;
+
+    // + операция сложения
+    // - операция вычитания
+    // * умножение
+    // / деление
+    // % получение остатка от деления
+
+    $int_op = 544;
+    $int_op_2 = '544.4444';
+
+    operators($int_op);
+    echo get_sum(1, 20);
+
+    if_else($int_op, $int_op_2);
 ?>
-</pre>
+</div>
+
+<?php
+require_once "php-loop.php";
+require_once "php-array.php";
+
+
+
+
+
 
