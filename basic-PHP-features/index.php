079ca66f51f896e4d9b9a7e3c2d901a818176daa
TykhonchukIvan <tykhonchukivan@gmail.com>
2020-05-19 23:57:07 +0300

    sring-method

--- basic-PHP-features/index.php
+++ basic-PHP-features/index.php
@@ -12,12 +12,19 @@
 <div class="section">
     <div class="section-form">
         <form method="POST" class="form-area">
+            <input name="name">
             <textarea name="text" class="form-area__text"></textarea>
             <button type="submit" class="form-area__btn">Отправить</button>
         </form>
     </div>
     <div class="section-answer">
-        <?php include "./php/processing-form-POST.php"; processing_form_POST();?>
+        <?php include "./php/processing-form-POST.php";
+//        echo_text($srt_text);
+//        echo_name($srt_name);
+        ?>
+    </div>
+    <div class="section-answer">
+        <?php work_on_string($srt_text,$srt_name); ?>
     </div>
 </div>
 </body>
